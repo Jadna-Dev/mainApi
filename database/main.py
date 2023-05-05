b3a1c3be00cfe8ba5bc5bdad5342c66632cfcfe6
jadna30 <108031359+jadna30@users.noreply.github.com>
2023-05-05 19:33:34 +0300

    sss

--- database/main.py
+++ database/main.py
@@ -7,36 +7,40 @@ database = FastAPI()
 
 @database.post("/insert_data/")
 async def insert_data(ldata: dict):
-    data = ldata["data"]
-    db.conn.database = data["onlineDbName"]
-    for d in data["data"]:
-        db.conn.rollback()
-        db.drop_table(data["onlineDbName"], d["tablename"])
-        sleep(1)
-        exec(f"""db.create_{d["tablename"]}(\"{data["onlineDbName"]}\")""")
-        print("here")
-        sleep(1)
-        for row in d["data"]:
-            fields = ""
-            values = ""
-            for idx, key in enumerate(row.keys()):
-                if idx == 0:
-                    fields = fields + "`"+key+"`"
-                else:
-                    fields = fields + "," + "`"+key+"`"
+    try:
+        data = ldata["data"]
+        db.conn.database = data["onlineDbName"]
+        for d in data["data"]:
+            db.conn.rollback()
+            db.drop_table(data["onlineDbName"], d["tablename"])
+            sleep(1)
+            exec(f"""db.create_{d["tablename"]}(\"{data["onlineDbName"]}\")""")
+            print("here")
+            sleep(1)
+            for row in d["data"]:
+                fields = ""
+                values = ""
+                for idx, key in enumerate(row.keys()):
+                    if idx == 0:
+                        fields = fields + "`"+key+"`"
+                    else:
+                        fields = fields + "," + "`"+key+"`"
 
-            for idx, val in enumerate(row.values()):
-                if idx == 0:
-                    values = values + "\'" + str(val).replace("\'", "")+"\'"
-                else:
-                    values = values + "," + "\'" + \
-                        str(val).replace("\'", "")+"\'"
-            print(
-                f"""INSERT INTO `{d["tablename"]}` ({fields}) VALUES ({values});""")
-            db.cur.execute(
-                f"""INSERT INTO `{d["tablename"]}` ({fields}) VALUES ({values});"""
-            )
-        db.conn.commit()
-    return{
-        "info":"successfull"
-    }
+                for idx, val in enumerate(row.values()):
+                    if idx == 0:
+                        values = values + "\'" + str(val).replace("\'", "")+"\'"
+                    else:
+                        values = values + "," + "\'" + \
+                            str(val).replace("\'", "")+"\'"
+                db.cur.execute(
+                    f"""INSERT INTO `{d["tablename"]}` ({fields}) VALUES ({values});"""
+                )
+            db.conn.commit()
+        return{
+            "info":"successfull"
+        }
+    except Exception as e:
+        return{
+            "info":"failed",
+            "msg":str(e)
+        }
