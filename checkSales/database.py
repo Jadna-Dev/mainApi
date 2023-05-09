5378dbdf81ffa5cd57b0912f26630716999e7bcd
jadna30 <108031359+jadna30@users.noreply.github.com>
2023-05-10 01:15:14 +0300

    ddd

--- checkSales/database.py
+++ checkSales/database.py
@@ -54,7 +54,8 @@ def select_data(dbname):
                    SELECT `pdxset`.name,pdxset.id,pdxgoods.itemcode,sum(CASE WHEN pdxinv.type = 'PI' or pdxinv.type = 'PIOPEN'   THEN  pdxinv.qprice * pdxinv.qin else 0 END) as total_cost
                         ,sum(CASE WHEN pdxinv.type = 'SA' THEN  pdxinv.qprice * pdxinv.qpacking else 0 END)-sum(CASE WHEN pdxinv.type = 'SR'   THEN  pdxinv.qprice * pdxinv.qpacking else 0 END) as total_sales
                         ,max(CASE WHEN pdxinv.type = 'PI' THEN  pdxinv.qprice  else 0 END ) * (sum(CASE WHEN pdxinv.type = 'SA'   THEN  pdxinv.qout  else 0 END) - sum(CASE WHEN pdxinv.type = 'SR'   THEN  pdxinv.qin else 0 END)) as sales_cost
-                        ,sum(CASE WHEN pdxinv.type = 'PIADJ' or pdxinv.type = 'PIOPEN' THEN  pdxinv.qin else 0 END) AS stockqty
+                        ,sum(CASE WHEN pdxinv.type = 'PIOPEN' THEN  pdxinv.qin else 0 END) AS openqty
+                        ,sum(CASE WHEN pdxinv.type = 'PIADJ' THEN  pdxinv.qin else 0 END) AS adjkqty
                                 FROM pdxset
                                 left JOIN `pdxgoods`
                                 ON pdxset.id = `pdxgoods`.set
@@ -91,7 +92,7 @@ def select_data(dbname):
             "total_sales": f"{round( total_sales):,}",
             "sales_cost": f"{round( sales_cost):,}",
             "sales_profit": f"{round( float(total_sales - sales_cost)):,}",
-            "stock_value": f"{float(round( float(total_cost - sales_cost) + (float(float(d['total_cost']) * float(d['stockqty']))))):,}",
+            "stock_value": f"{float(round( float((total_cost - sales_cost) + (float(float(d['total_cost']) * float(d['openqty']))) - (float(float(d['total_cost']) * float(d['adjkqty']))) ))):,}",
         })
     return fdata
 
