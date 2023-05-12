1878c43ae07b64ca02883daa0d305c607b0a7138
jadna30 <108031359+jadna30@users.noreply.github.com>
2023-05-12 19:22:34 +0300

    ddd

--- checkSales/database.py
+++ checkSales/database.py
@@ -85,14 +85,14 @@ def select_data(dbname):
                 total_cost = total_cost + float(d["total_cost"])
                 total_sales = total_sales + float(d["total_sales"])
                 sales_cost = float(sales_cost) + float(d["sales_cost"])
-                qtyvalue =  (float(d['adjkqty']))
+                qtyvalue =  float(d['adjkqty'])
         fdata.append({
             "setname": setid["name"],
             "total_cost": f"{round( total_cost):,}",
             "total_sales": f"{round( total_sales):,}",
             "sales_cost": f"{round( sales_cost):,}",
             "sales_profit": f"{round( float(total_sales - sales_cost)):,}",
-            "stock_value": f"{round( float((total_cost - sales_cost) + qtyvalue )):,}",
+            "stock_value": f"{round( float((total_cost - sales_cost) - qtyvalue )):,}",
         })
     return fdata
 
