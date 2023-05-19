9f911a16e45ff3fcc899a2ca8820cc9175504157
jadna30 <108031359+jadna30@users.noreply.github.com>
2023-05-19 10:36:02 +0300

    ddd

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ async def home():
 if __name__ == "__main__":
     with open("./config.json", "r") as conf:
         config = eval(str(conf.read()))
-    uvicorn.run("main:app", host=config["host"], port=config["port"],reload=True)
+    uvicorn.run("main:app", host=config["host"], port=config["port"],reload=True,workers=4)
